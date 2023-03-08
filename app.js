cfb11c536149cda0548b9cba1a5f9551e5d7176b
Joey Douek <joeyblue27@gmail.com>
2023-03-08 15:32:46 -0500

    app.js lib/js

--- app.js
+++ app.js
@@ -2,34 +2,161 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const render = require("./lib/htmlRenderer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const members = [];
+
+const questions = () => {
+    console.log('Lets Build your team!');
+    return inquirer.prompt([
+        {
+            type: 'list',
+            question: "What is the employee's position?",
+            name: 'position',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            question: "What is the managers team name?",
+            name: 'teamName',
+            when: (answers) => answers.position === 'Manager',
+        },
+        {
+            type: 'input',
+            question: "What is the managers team id?",
+            name: 'managerId',
+            when: (answer) => answer.position === 'Manager',
+        },
+        {
+            type: 'input',
+            question: "What is the managers email?",
+            name: 'managerEmail',
+            when: (answer) => answer.position === 'Manager',
+        },
+        {
+            type: 'input',
+            question: "What is the team managers office number?",
+            name: 'managerOffice',
+            when: (answer) => answer.position === 'Manager',
+        },
+        {
+            type: 'input',
+            question: "What is the engineers name?",
+            name: 'engineerName',
+            when: (answer) => answer.position === 'Engineer',
+        },
+        {
+            type: 'input',
+            question: "What is the engineers id?",
+            name: 'engineerId',
+            when: (answer) => answer.position === 'Engineer',
+        },
+        {
+            type: 'input',
+            question: "What is the engineers email?",
+            name: 'engineerEmail',
+            when: (answer) => answer.position === 'Engineer',
+        },
+        {
+            type: 'input',
+            question: "What is the engineers Github?",
+            name: 'engineergithub',
+            when: (answer) => answer.position === 'Engineer',
+        },
+        {
+            type: 'input',
+            question: "What is the interns name?",
+            name: 'internName',
+            when: (answer) => answer.position === 'Intern',
+        },
+        {
+            question: "What is the interns id?",
+            name: 'internId',
+            when: (answer) => answer.position === 'Intern',
+        },
+        {
+            type: 'input',
+            question: "What is the interns email?",
+            name: 'internEmail',
+            when: (answer) => answer.position === 'Intern',
+        },
+        {
+            type: 'input',
+            question: "Where is the interns school?",
+            name: 'internSchool',
+            when: (answer) => answer.position === 'Intern',
+        },
+        {
+            type: 'confirm',
+            question: 'Add another member?',
+            name: 'anotherMember',
+        },
+    ])
+
+        .then((answer) => {
+            var team;
+
+            if (answer.position === 'Manager') {
+                team = new Manager(answer.teamName,
+                    answer.managerId,
+                    answer.managerEmail,
+                    answer.managerOffice);
+
+            } else if (answer.position === 'Engineer') {
+                team = new Engineer(answer.engineerName,
+                    answer.engineerId,
+                    answer.engineergithub,
+                    answer.engineerEmail);
+
+            } else if (answer.position === 'Intern') {
+                team = new Intern(answer.internName,
+                    answer.internId,
+                    answer.internSchool,
+                    answer.internEmail);
+            }
+
+            members.push(team);
+            if (answer.anotherMember) {
+                return questions();
+            } else {
+                return members;
+            }
+        })
+}
+
+function writeFileAsync(copy, write) {
+    return new Promise((createFile) => {
+
+        if (!fs.existsSync(DIST_DIR)) {
+            fs.mkdirSync(DIST_DIR);
+        }
+
+        fs.writeFile(copy, write, function (render) {
+            if (render) {
+
+                return;
+            }
+            createFile();
+        });
+    });
+}
+
+questions()
+    .then((members) => {
+        return render(members);
+    })
+    .then((file) => {
+        return writeFileAsync(distPath, file);
+    })
+    .then(() => {
+        console.log('All Done!');
+    })
+    .catch((err) => console.error(err));
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
